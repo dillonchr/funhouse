5f0fea2ae8451ad3ecfb571364f463c4b74fc7aa
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-02-06 22:15:20 -0600

    updated bookmancy to allow options to be used

--- components/bookmancy/bookmancy.js
+++ components/bookmancy/bookmancy.js
@@ -3,13 +3,15 @@ const bookmancy = require('bookmancy');
 module.exports = (options, onResponse) => {
     let promise;
     if (options.source === 'ebay') {
-        const ebayOpts = Object.entries(options)
+        const query = options.query || Object.entries(options)
             .filter(([key]) => ['author', 'title', 'publisher', 'year'].includes(key))
             .map(([k, val]) => val)
             .join(' ');
-        promise = bookmancy.ebay.search(ebayOpts)
+        const call = options.sold ? bookmancy.ebay.searchSoldListings : (options.live ? bookmancy.ebay.searchLiveListings : bookmancy.ebay.search);
+        promise = call(query);
     } else {
-        promise = bookmancy.abe.search(options);
+        const call = options.withUrl ? bookmancy.abe.searchWithUrlInResponse : bookmancy.abe.search;
+        promise = call(options);
     }
     promise
         .then(results => onResponse(null, results))
