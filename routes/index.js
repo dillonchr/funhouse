00903e41898da9a6a32da35a495f0b048fe527b4
Dillon Christensen <j.dillon.christensen@gmail.com>
2023-03-23 15:15:50 -0500

    removed redundant budget handler and updated bankrupt

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports = [
   require("./bookmancy.js"),
-  require("./budget.js"),
   require("./cryptonics.js"),
   require("./dailytext.js"),
   require("./gdq.js"),
--- routes/paycheck.js
+++ routes/paycheck.js
@@ -16,12 +16,12 @@ module.exports = (req, res) => {
       } else if (req.body.amount) {
         onBalanceResponse(res, paycheck.spend(id, req.body.amount));
       } else {
-        sendResponse(res, 400, toError("paycheck pay requires an amount"));
+        sendResponse(res, 400, toError(`missing data for ${req.method} call to paycheck (id, description, amount are all required)`));
       }
     }
   }
 };
 
 module.exports.shouldRoute = req =>
-  req.hasAccess("paycheck") && /^\/paycheck/.test(req.url);
+  req.hasAccess("paycheck") && (/^\/paycheck/.test(req.url) || /^\/budget/.test(req.url));
 module.exports.init = paycheck.init;
